1ca290cb1572bc571fe6f226c494207853d2b7fa
argo46 <adityaargo4609@gmail.com>
2019-12-07 00:27:40 +0700

    fix update company file upload

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -1,86 +1,107 @@
-const companyModels = require('../models/company')
-const uuid4 = require('uuid/v4')
+const companyModels = require("../models/company");
+const uuid4 = require("uuid/v4");
 
 module.exports = {
   getCompanies: (req, res) => {
-    companyModels.getCompanies()
-      .then((result) => {
-        res.json(result)
+    companyModels
+      .getCompanies()
+      .then(result => {
+        res.json(result);
       })
-      .catch((err) => {
-        console.log(err)
-      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   getCompany: (req, res) => {
-    const { id } = req.params
-    companyModels.getCompany(id)
-      .then((result) => {
-        res.json(result)
+    const { id } = req.params;
+    companyModels
+      .getCompany(id)
+      .then(result => {
+        res.json(result);
       })
-      .catch((err) => {
-        console.log(err)
-      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   addCompany: (req, res) => {
-    const data = req.body
-    data.id = uuid4()
+    const data = req.body;
+    data.id = uuid4();
     if (!req.file) {
       res.json({
         success: false,
-        message: 'No company logo uploaded or file format is not supported'
-      })
-      return
+        message: "No company logo uploaded or file format is not supported"
+      });
+      return;
     } else {
-      const host = req.hostname
-      req.file.filename = data.name + req.file.filename
-      const filePath = req.protocol + '://' + host + ':' + process.env.PORT + '/' + req.file.path
-      data.logo = filePath
+      const host = req.hostname;
+      req.file.filename = data.name + req.file.filename;
+      const filePath =
+        req.protocol +
+        "://" +
+        host +
+        ":" +
+        process.env.PORT +
+        "/" +
+        req.file.path;
+      data.logo = filePath;
     }
 
-    companyModels.addCompany(data)
+    companyModels
+      .addCompany(data)
       .then(() => {
         res.json({
-          message: 'success',
+          message: "success",
           data
-        })
+        });
       })
-      .catch((err) => {
-        console.log(err)
-      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   updateCompany: async (req, res) => {
-    const data = await req.body
-    const { id } = req.params
-    console.log(data)
+    const data = await req.body;
+    const { id } = req.params;
+    console.log(data);
 
-    const host = req.hostname
-    req.file.filename = data.name + req.file.filename
-    const filePath = req.protocol + '://' + host + ':' + process.env.PORT + '/' + req.file.path
-    data.logo = filePath
+    if (req.file) {
+      const host = req.hostname;
+      req.file.filename = data.name + req.file.filename;
+      const filePath =
+        req.protocol +
+        "://" +
+        host +
+        ":" +
+        process.env.PORT +
+        "/" +
+        req.file.path;
+      data.logo = filePath;
+    }
 
-    companyModels.updateCompany(id, data)
+    companyModels
+      .updateCompany(id, data)
       .then(() => {
         res.json({
-          message: 'success',
+          message: "success",
           data
-        })
+        });
       })
-      .catch((err) => {
-        console.log(err)
-      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   deleteCompany: (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
     // TODO validate delete if is exist
-    companyModels.deleteCompany(id)
-      .then((result) => {
+    companyModels
+      .deleteCompany(id)
+      .then(result => {
         res.json({
-          message: 'success',
+          message: "success",
           id
-        })
+        });
       })
-      .catch((err) => {
-        console.log(err)
-      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-}
+};
